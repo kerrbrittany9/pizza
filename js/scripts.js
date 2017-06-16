f9fad3d375d145a6cd534967f64822203c9f6230
Brittany Kerr <kerrbrittany9@gmail.com>
2017-06-16 09:44:08 -0700

    add jquery and html for order to appear on page

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-function Pizza(protein) {
+function Pizza(first, last, protein, veggies) {
   this.firstName = first;
   this.lastName = last;
   this.protein = protein;
@@ -6,6 +6,9 @@ function Pizza(protein) {
 
 }
 
+Pizza.prototype.fullOrder = function() {
+  return this.firstName + " " + this.lastName + " " + this.protein + " " + this.veggies;
+}
 
 
 $(document).ready(function() {
@@ -22,16 +25,9 @@ $(document).ready(function() {
 
     var newPizza = new Pizza(inputtedFirstName, inputtedLastName, inputtedProtein, inputtedVeggies);
 
-
-    $("#protein").append(inputtedProtein + "<br>");
-
-  });
-
-
-    $("#vegetables").show();
-
-     $("#vegetables").append(inputtedVeggies + "<br>");
+  $("ul#order").append("<li>" + newPizza.fullOrder() + "</span></li>");
 
     });
   });
+});
 });
