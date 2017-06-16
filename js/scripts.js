f19716a029afe0d63effdef3717c3856a8eddb90
Brittany Kerr <kerrbrittany9@gmail.com>
2017-06-16 10:54:08 -0700

    streamline protein/veggies to toppings

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,37 @@
-function Pizza(first, last, size, protein, veggies) {
+function Pizza(first, last, size, toppings) {
   this.firstName = first;
   this.lastName = last;
-  this.protein = protein;
-  this.veggies = veggies;
   this.pizzaSize = size;
-
+  this.toppings = toppings;
 }
 
 Pizza.prototype.fullOrder = function() {
-  return this.firstName + " " + this.lastName + " " + this.pizzaSize + " " + this.protein + " " + this.veggies;
+  return this.firstName + " " + this.lastName + " " + this.pizzaSize + " " + this.toppings;
 }
 
+function resetFields() {
+  $("#new-first-name").val("");
+  $("#new-last-name").val("");
+  $("#size").val("small");
+  // $("#toppings").val("unchecked");
+}
 
 $(document).ready(function() {
   $("form#pizza").submit(function(event) {
   event.preventDefault();
 
-    var inputtedSize = $("#size").val()
-    debugger
 
+    var inputtedSize = $("#size").val()
     var inputtedFirstName = $("input#new-first-name").val();
     var inputtedLastName = $("input#new-last-name").val();
-    $("input:checkbox[name=protein]:checked").each(function(){
-    var inputtedProtein= $(this).val();
-      $("input:checkbox[name=vegetables]:checked").each(function(){
-    var inputtedVeggies= $(this).val();
+    $("input:checkbox[name=toppings]:checked").each(function(){
+    var inputtedToppings= $(this).val();
 
-
-
-    var newPizza = new Pizza(inputtedFirstName, inputtedLastName, inputtedSize, inputtedProtein, inputtedVeggies);
+    var newPizza = new Pizza(inputtedFirstName, inputtedLastName, inputtedSize, inputtedToppings);
 
   $("ul#order").append("<li>" + newPizza.fullOrder() + "</span></li>");
 
     });
+  resetFields();
   });
-});
 });
